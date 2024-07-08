33cabc7c5dd199b4d25d82f93474e84351a49366
devgyuri <gksmf910@gmail.com>
2024-07-08 17:41:53 +0900

    feat: spring boot setting

---
+++ src/main/java/me/gyuri/tripity/TripityApplication.java
@@ -0,0 +1,11 @@
+package me.gyuri.tripity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TripityApplication.class, args);
+    }
+}
